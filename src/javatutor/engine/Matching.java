d9e8d91c35ea0bbb67a6dce341321d155a500a4f
Bernát Kalló <kallo.bernat@gmail.com>
2020-02-27 15:09:57 +0100

    implemented normal variables and statement variables

--- src/javatutor/engine/Matching.java
+++ src/javatutor/engine/Matching.java
@@ -1,20 +1,29 @@
 package javatutor.engine;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.jdt.core.dom.ASTMatcher;
 import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.Block;
+import org.eclipse.jdt.core.dom.Assignment;
 import org.eclipse.jdt.core.dom.ExpressionStatement;
 import org.eclipse.jdt.core.dom.SimpleName;
 
-import javatutor.engine.Matching.UnknownNodeTypeException;
-
 public class Matching {
 
-	public static class UnknownNodeTypeException extends RuntimeException {
+	public static class InvalidVariableException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
+		public final String varName;
+		public InvalidVariableException(String varName) {
+			super("Invalid variable: " + varName);
+			this.varName = varName;
+		}
+	}
 
+	public static class UnknownNodeTypeException extends RuntimeException {
+		private static final long serialVersionUID = 1L;
 		public final String typeName;
 
 		public UnknownNodeTypeException(String typeName) {
@@ -25,16 +34,7 @@ public class Matching {
 	}
 
 	static class Match {
-		
-		int position;
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + position;
-			return result;
-		}
+		Map<String, ASTNode> bindings = new HashMap<>();
 
 		@Override
 		public boolean equals(Object obj) {
@@ -45,39 +45,84 @@ public class Matching {
 			if (getClass() != obj.getClass())
 				return false;
 			Match other = (Match) obj;
-			if (position != other.position)
+			if (!bindings.equals(other.bindings))
 				return false;
 			return true;
 		}
 		
+
 	}
-	
+
 	private static class PatternMatcher extends ASTMatcher {
-		static final Pattern WILDCARD_VARIABLE = Pattern.compile("\\$([a-zA-Z]+)");
-		Match match = new Match();
+		private static final Pattern VARIABLE_LIKE = Pattern.compile("\\$.*");
+		private static final Pattern WILDCARD_VARIABLE = Pattern.compile("\\$([a-zA-Z]+)");
+		private static final Pattern NORMAL_VARIABLE = Pattern.compile("\\$(([a-zA-Z]+)[0-9]+)");
+		private final Match match = new Match();
+
+		public Match getMatch() {
+			return match;
+		}
 
 		@Override
 		public boolean match(SimpleName node, Object other) {
+			String name = node.getIdentifier();
+			if (!VARIABLE_LIKE.matcher(name).matches()) return super.match(node, other);
 			Matcher m;
-			if ((m = WILDCARD_VARIABLE.matcher(node.getIdentifier())).matches()) {
-				String typeName = m.group(1);
-				Class<?> clazz;
-				try {
-					clazz = getClass().getClassLoader().loadClass("org.eclipse.jdt.core.dom." + typeName);
-				} catch (ClassNotFoundException e) {
-					throw new UnknownNodeTypeException(typeName);
-				}
-				return clazz.isInstance(other);
+			if ((m = WILDCARD_VARIABLE.matcher(name)).matches()) {
+				return findASTNodeClass(m.group(1)).isInstance(other);
 			}
-			return super.match(node, other);
+			if ((m = NORMAL_VARIABLE.matcher(name)).matches()) {
+				String varName = m.group(1), typeName = m.group(2);
+				if (!findASTNodeClass(typeName).isInstance(other)) return false;
+				ASTNode otherNode = (ASTNode) other;
+				if (!match.bindings.containsKey(varName)) {
+					match.bindings.put(varName, otherNode);
+					return true;
+				} else {
+					// check equality with the stored value
+					return match.bindings.get(varName).subtreeMatch(new ASTMatcher(), otherNode);
+				}
+			}
+			throw new InvalidVariableException(name);
 		}
-		
-		public Match getMatch() {
-			return new Match();
+		@Override
+		public boolean match(ExpressionStatement node, Object other) {
+			return matchStatementVariable(node, other) || super.match(node, other);
 		}
-		
+
+		private boolean matchStatementVariable(ExpressionStatement node, Object other) {
+			if (node.getExpression() instanceof SimpleName) {
+				SimpleName name = (SimpleName) node.getExpression();
+				return match(name, other);
+			}
+			if (!(node.getExpression() instanceof Assignment)) return false;
+			Assignment a = (Assignment) node.getExpression();
+			if (!(a.getRightHandSide() instanceof SimpleName)) return false;
+			SimpleName right = (SimpleName) a.getRightHandSide();
+			if (!right.getIdentifier().equals("$missing$")) return false;
+			// this happens if a single identifier is there instead of a statement
+			if (!(a.getLeftHandSide() instanceof SimpleName)) return false;
+			SimpleName left = (SimpleName) a.getLeftHandSide();
+			// continue as if only the left hand side were there
+			return match(left, other);
+		}
+
+		@SuppressWarnings("unchecked")
+		private static Class<? extends ASTNode> findASTNodeClass(String typeName) {
+			Class<?> clazz;
+			try {
+				clazz = PatternMatcher.class.getClassLoader().loadClass("org.eclipse.jdt.core.dom." + typeName);
+			} catch (ClassNotFoundException e) {
+				throw new UnknownNodeTypeException(typeName);
+			}
+			if (!ASTNode.class.isAssignableFrom(clazz)) {
+				throw new UnknownNodeTypeException(typeName);
+			}
+			return (Class <? extends ASTNode>) clazz;
+		}
+
+
 	}
-	
 
 	public static Match match(ASTNode pattern, ASTNode concrete) throws UnknownNodeTypeException {
 		PatternMatcher matcher = new PatternMatcher();
--- src/javatutor/engine/MatchingTest.java
+++ src/javatutor/engine/MatchingTest.java
@@ -1,21 +1,31 @@
 package javatutor.engine;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+import javatutor.engine.Matching.InvalidVariableException;
 import javatutor.engine.Matching.Match;
+import javatutor.engine.Matching.UnknownNodeTypeException;
 
 class MatchingTest {
 
-	private void check(Match expected, String pattern, String concrete) {
+	private void check(String expected, String pattern, String concrete) {
 		Match match = Matching.match(Engine.parseStatements(pattern), Engine.parseStatements(concrete));
-		assertEquals(expected, match);
+		if (match == null) {
+			assertEquals(expected, null);
+		} else {
+			assertEquals(expected, match.bindings.toString());
+		}
 	}
+
+	// -- no variables
 
 	@Test
 	void noVariables_same() {
-		check(new Match(), "f()", "f()");
+		check("{}", "f()", "f()");
 	}
 
 	@Test
@@ -27,10 +37,54 @@ class MatchingTest {
 	void noVariables_differentNames() {
 		check(null, "f()", "g()");
 	}
+	
+	// -- invalid patterns
+	@Test
+	void unknownType() {
+		assertThrows(UnknownNodeTypeException.class, () -> check("", "$blah()", "g()"));
+	}
+
+	@Test
+	void invalidVariable() {
+		assertThrows(InvalidVariableException.class, () -> check("", "$invalid_()", "g()"));
+	}
+	
+	// -- wildcard variables
 
 	@Test
 	void wildcardVariable_functionName() {
-		check(new Match(), "$SimpleName()", "g()");
+		check("{}", "$SimpleName()", "g()");
+	}
+	
+	// -- normal variables
+	
+	@Test
+	void variable_once() {
+		check("{SimpleName1=g}", "$SimpleName1()", "g()");
+	}
+	@Test
+	void variable_twice() {
+		check("{Expression1=3}", "Math.max($Expression1,$Expression1)", "Math.max(  3, 3)");
+		check(null,              "Math.max($Expression1,$Expression1)", "Math.max(  3, 4)");
+	}
+	
+	// -- statement variables
+	@Test
+	void statementVariable_expression() {
+		check("{Statement1=f();\n}", "if (true) $Statement1", "if (true) f();");
+		check("{Statement1=f();\n}", "if (true) $Statement1", "if (true) f()");
+		check("{Statement1=f();\n}", "if (true) $Statement1;", "if (true) f()");
+		check("{Statement1=f();\n}", "if (true) $Statement1;", "if (true) f();");
+	}
+	@Test
+	void statementVariable_block() {
+		check("{Statement1={\n  f();\n}\n}", "if (true) $Statement1", "if (true) { f(); }");
+	}
+	@Test
+	void statementVariable_if() {
+		check("{Statement1=if (hello) f();\n}", "if (true) $Statement1", "if (true) if (hello) f();");
+		check("{IfStatement1=if (hello) f();\n}", "if (true) $IfStatement1", "if (true) if (hello) f();");
+		check(null, "if (true) $IfStatement1", "if (true) while (hello) f();");
 	}
 
 }
