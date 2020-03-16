d2885efbfd2bab2ded8d54cab847cf178a4ed47d
Bernát Kalló <kallo.bernat@gmail.com>
2020-03-16 07:01:43 +0100

    add splash svg, close views on start

--- src/javatutor/intro/JavaTutorIntro.java
+++ src/javatutor/intro/JavaTutorIntro.java
@@ -4,10 +4,16 @@ import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.browser.OpenWindowListener;
 import org.eclipse.swt.browser.WindowEvent;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.intro.IIntroPart;
 import org.eclipse.ui.part.IntroPart;
 
 public class JavaTutorIntro extends IntroPart {
+
+	private Browser browser;
 
 	@Override
 	public <T> T getAdapter(Class<T> adapter) {
@@ -23,38 +29,36 @@ public class JavaTutorIntro extends IntroPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		Browser browser = new Browser(parent, 0);
+		browser = new Browser(parent, 0);
 		browser.setText("<h2>Hello!</h2><p>This is a survey. The task is:</p>"
 				+ "<h3>Analyze scores</h3><p>Write a program that reads an unspecified number of "
-						+ "scores and determines how many scores are above or equal to the "
-						+ "average and how many scores are below the average. "
-						+ "Enter a negative number to signify the end of the input. "
-						+ "Assume that the maximum number of scores is 100.</p><p>"
+				+ "scores and determines how many scores are above or equal to the "
+				+ "average and how many scores are below the average. "
+				+ "Enter a negative number to signify the end of the input. "
+				+ "Assume that the maximum number of scores is 100.</p><p>"
 				+ "<button onclick='window.open();return false'>Let's start!</button>");
 		browser.addOpenWindowListener(new OpenWindowListener() {
 			@Override
 			public void open(WindowEvent event) {
-				PlatformUI.getWorkbench().getIntroManager().setIntroStandby(JavaTutorIntro.this, true);
-				browser.setText(
-						"<h3>Analyze scores</h3>Write a program that reads an unspecified number of "
-						+ "scores and determines how many scores are above or equal to the "
-						+ "average and how many scores are below the average. "
-						+ "Enter a negative number to signify the end of the input. "
-						+ "Assume that the maximum number of scores is 100.");
+				letsStart();
 			}
 		});
-		// Button button = new Button(browser, 0);
-		// button.addSelectionListener(new SelectionListener() {
+	}
 
-		// @Override
-		// public void widgetSelected(SelectionEvent e) {
-		// }
+	protected void letsStart() {
+		PlatformUI.getWorkbench().getIntroManager().setIntroStandby(JavaTutorIntro.this, true);
+		IWorkbenchPage page = this.getIntroSite().getWorkbenchWindow().getActivePage();
+		for (IViewReference view : page.getViewReferences()) {
+			IViewPart v = view.getView(false);
+			if (v != null && v.getAdapter(IIntroPart.class) != null) continue;
+			page.hideView(view);
+		}
+		browser.setText("<h3>Analyze scores</h3>Write a program that reads an unspecified number of "
+				+ "scores and determines how many scores are above or equal to the "
+				+ "average and how many scores are below the average. "
+				+ "Enter a negative number to signify the end of the input. "
+				+ "Assume that the maximum number of scores is 100.");
 
-		// @Override
-		// public void widgetDefaultSelected(SelectionEvent e) {
-		//// TODO Auto-generated method stub
-		// }
-		// });
 	}
 
 	@Override
